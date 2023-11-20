2918e05174ab376122482e6c1440ac0942dae878
saiki <takanari-saiki@upload-gp.co.jp>
2023-11-20 12:27:42 +0900

    クリックイベントを取得するロジックを修正

--- src/injects/printClickEvents.ts
+++ src/injects/printClickEvents.ts
@@ -4,5 +4,26 @@
  * @param {string} selector
  */
 export function printClickEvents(selector) {
-    console.log(frevo.getEventHandlers($(selector)[0]));
+    const target = $(selector).first()
+    target.parents().each((_, elem) => {
+        if (elem.tagName === 'html') {
+            elem = document;
+        }
+        const events = $(elem).data('events');
+        if (!events || !events.click) return true;
+        events.click.forEach(item => {
+            /** @type {string} */
+            const selector = item.selector;
+            if (!selector) return true;
+    
+            // selectorはjqueryのものであるため、$()を必ず使用する
+            $(selector).each((_, elem2) => {
+                if (elem2.isEqualNode(target[0]) 
+                    || elem2.contains(target[0])
+                ) {
+                    console.log(item.selector, item)
+                }
+            })
+        })
+    })
 }
